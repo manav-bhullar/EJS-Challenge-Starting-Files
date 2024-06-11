e67e7a3db931bd17532fd7f95004ebf50c067427
NEW-BORN <manavbhullar341@gmail.com>
2024-06-11 19:20:44 +0530

    chore: Update home.ejs to display posts dynamically

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -17,7 +17,10 @@ app.use(express.static("public"));
 var posts = [];
 
 app.get("/", function(req,res){
-  res.render("home", {startingContent: homeStartingContent});
+  res.render("home", {startingContent: homeStartingContent, 
+    posts: posts
+  });
+
 })
 app.get("/about",function(req,res){
   res.render("about", {about: aboutContent});
